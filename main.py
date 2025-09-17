19d0f078802e2f1c6d45a30c6e570258f0621424
ilina.iyer <ilina.iyer@gmail.com>
2025-09-16 23:36:35 -0700

    remove secret

--- main.py
+++ main.py
@@ -1,1 +1,57 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+#from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
+
+load_dotenv("/Users/ilinaiyer/My-Learning-Assistant/sample.env")
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0.7
+)
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system",
+        """
+        You are a 1 on 1 tutor for speech therapy, give me multiple choice reading comprehension questions
+        """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+tools = [search_tool, wiki_tool, save_tool]
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=tools
+)
+
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("What can I help you with...")
+raw_response = agent_executor.invoke({"query": query})
+
+raw_response = agent_executor.invoke({"query": query})
+
+try: 
+    structured_response = parser.parse(raw_response["output"])
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_response)
+
 
